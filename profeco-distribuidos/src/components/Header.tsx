f43b787d2e421adf498cd116451a89a799c41295
Chinchillas <79560217+chinchillas03@users.noreply.github.com>
2024-11-15 13:04:13 -0700

    Header app web profeco

---
+++ profeco-distribuidos/src/components/Header.tsx
@@ -0,0 +1,22 @@
+export default function Header() {
+    return (
+        <header className="py-5 header">
+            <div className="container-xl">
+                <div className="row justify-content-center justify-content-md-between">
+                    <div className="col-8 col-md-3">
+                        <a href="index.html">
+                            <img className="img-fluid" src="/public/img/logo-profeco.png" alt="imagen logo" />
+                        </a>
+                    </div>
+                    <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
+                        <div
+                            className="carrito"
+                        >
+                            <img className="img-fluid" src="/public/img/carrito.png" alt="imagen carrito" />
+                        </div>
+                    </nav>
+              </div>
+          </div>
+      </header>
+    )
+}
