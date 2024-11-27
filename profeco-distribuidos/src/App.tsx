86ef7568b94422baec6d7a52015e2c659021ed3e
waw89 <lcontrerasperaza99@gmail.com>
2024-11-27 06:06:48 -0700

    reconfiguracion del proyecto, TS, Bootstrap, React

--- profeco-distribuidos/src/App.tsx
+++ profeco-distribuidos/src/App.tsx
@@ -1,39 +1,26 @@
-
-import Header from './components/Header'
-import Product from './components/Product'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
   return (
-    <>
-      <Header />
-      <main className="container-xl mt-5">
-        <h2 className='text-center text-uppercase mt-3'>Productos disponibles</h2>
-        <form className="d-flex justify-content-md-between ">
-          <input
-            className="form-control "
-            type="search"
-            placeholder="Buscar productos..."
-            aria-label="Buscar"
-          />
-          <button className="btn btn-outline-primary" type="submit">Buscar</button>
-        </form>
-        <div className='row mt-3'>
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-        </div>
-      </main>
-      <footer className="bg-dark mt-5 py-5">
-        <div className="container-xl">
-          <p className="text-white text-center fs-4 mt-4 m-md-0">Profeco - Todos los derechos Reservados</p>
-        </div>
-      </footer>
-    </>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
