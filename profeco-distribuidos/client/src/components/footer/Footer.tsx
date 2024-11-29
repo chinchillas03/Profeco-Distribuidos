60830d892354e90a4741b537d4911eb9311ba77e
waw89 <lcontrerasperaza99@gmail.com>
2024-11-29 01:09:59 -0700

    reorganization of the folders

---
+++ profeco-distribuidos/client/src/components/footer/Footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+    return (
+      <footer className="page-footer text-light">
+        <div className="container">
+          <div className="row">
+
+            
+        
+          </div>
+
+        </div>
+      </footer>
+    );
+  }
